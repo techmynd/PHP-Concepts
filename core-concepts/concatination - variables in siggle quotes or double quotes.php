14933dfe63ec534ff21d560e455f386a1d839487
javed khalil <admin@techmynd.com>
2019-02-01 12:03:27 +0500

    update

---
+++ core-concepts/concatination - variables in siggle quotes or double quotes.php
@@ -0,0 +1,9 @@
+$name = "Mario";
+
+// correct
+echo "My name is $name";
+
+// wrong
+echo 'My name is $name';
+
+// double quotes can have variable
---
+++ core-concepts/constants.php
@@ -0,0 +1,12 @@
+<?php 
+
+define('SERVER', 'Heroku');
+// SERVER is constant and Heroku is its value
+// Note that SERVER is all caps
+
+echo SERVER;
+
+// you can not change constant value
+
+?>
+
